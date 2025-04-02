ea2514b8e034cfeac36f3a8ee18304314fe8d40d
VBPMAD <vbpmad@gmail.com>
2025-04-02 16:38:03 -0600

    prueba_SATP_3

--- pruebadisplayCode_SATP_2025.js
+++ pruebadisplayCode_SATP_2025.js
@@ -1,28 +1,49 @@
 function displayCodeSATP() {
+  let activeElementId = loader.engine.document.activeElement.id;
 
-  switch (loader.engine.document.activeElement.id) {
-    case 61871077:
-      var valueOne = loader.engine.document.getElementById(61871077).getProperty('value.value');
-  
-           case "ARGENTINA": valueCode = "54"; break;
-case "BOLIVIA":valueCode = "591"; break;
-case "BRASIL":valueCode = "55"; break;
-case "CHILE":valueCode = "56"; break;
-case "ECUADOR":valueCode = "593"; break;
-case "NICARAGUA":valueCode = "505"; break;
-case "PARAGUAY":valueCode = "595"; break;
-case "PERU":valueCode = "51"; break;
-case "URUGUAY":valueCode = "598"; break;
-default: valueCode = ""; break;
-   
-      loader.engine.document.getElementById(61871085).setValue(({"value": valueCode}));
+  switch (activeElementId) {
+    case 61871077: {
+      let valueCode = "";
+      let element = loader.engine.document.getElementById(61871077);
+
+      if (element) {
+        let valueOne = element.getProperty('value.value');
+        switch (valueOne) {
+          case "ARGENTINA": valueCode = "54"; break;
+          case "BOLIVIA": valueCode = "591"; break;
+          case "BRASIL": valueCode = "55"; break;
+          case "CHILE": valueCode = "56"; break;
+          case "ECUADOR": valueCode = "593"; break;
+          case "NICARAGUA": valueCode = "505"; break;
+          case "PARAGUAY": valueCode = "595"; break;
+          case "PERU": valueCode = "51"; break;
+          case "URUGUAY": valueCode = "598"; break;
+          default: valueCode = ""; break;
+        }
+      }
+
+      loader.engine.document.getElementById(61871085)?.setValue({ "value": valueCode });
       break;
-   case 61871084:
-     var valueOne = loader.engine.document.getElementById(61871084).getProperty('value.value');
-     loader.engine.document.getElementById(61871084).setValue(({"value": valueOne.toLowerCase()})); 
-     if (loader.engine.document.rootNode[34].offsetParent != null)
-       loader.engine.document.getElementById(97747666).visible = false;
-     break;
+    }
+
+    case 61871084: {
+      let element = loader.engine.document.getElementById(61871084);
+
+      if (element) {
+        let valueOne = element.getProperty('value.value');
+        element.setValue({ "value": valueOne.toLowerCase() });
+      }
+
+      if (loader.engine.document.rootNode[34]?.offsetParent != null) {
+        loader.engine.document.getElementById(97747666).visible = false;
+      }
+      break;
+    }
+
+    default:
+      console.warn("No matching case for the active element ID.");
   }
 }
-window.onchange = displayCodeSATP;
+
+// Use addEventListener for better cross-browser compatibility
+window.addEventListener('change', displayCodeSATP);
